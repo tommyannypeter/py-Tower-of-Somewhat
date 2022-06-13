b4c4e3566496f8c8ca9f6efe9bb7c7af7161710f
Shaoluen Huang <shaoluen16@gmail.com>
2022-06-13 16:23:39 +0900

    remove setColor function, changed to intialize in __init__

--- Ball.py
+++ Ball.py
@@ -6,10 +6,7 @@ import random
 
 class Ball:
     def __init__(self):
-        self.color = self.setColor()
-
-    def setColor(self):
-        return random.choice([Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.PURPLE])
+        self.color = random.choice([Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.PURPLE])
 
     def __repr__(self):
         return str(self.color.name)
