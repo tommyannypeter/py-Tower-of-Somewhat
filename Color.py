e405408e915de012526ad288da902e77259e1464
Shaoluen Huang <shaoluen16@gmail.com>
2022-06-13 15:12:42 +0900

    remove import random from Color.py

--- Color.py
+++ Color.py
@@ -1,5 +1,4 @@
 from enum import Enum
-import random
 
 class Color(Enum):
     BLACK = (0, 0, 0)
@@ -9,6 +8,3 @@ class Color(Enum):
     BLUE = (0, 0, 255)
     YELLOW = (255, 255, 0)
     PURPLE = (255, 0, 255)
-
-    def get_random_color():
-        return random.choice([Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.PURPLE])
