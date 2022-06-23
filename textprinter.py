96d096465b7a7b04854d8114c774da9036a19aec
Po-Shan Shih <tommyannypeter@gapp.nthu.edu.tw>
2022-06-23 21:32:40 +0800

    separate text printer

---
+++ textprinter.py
@@ -0,0 +1,34 @@
+import pygame
+
+from color import Color
+
+class TextPrinter:
+    def __init__(self, text: str, font_size: int, position: tuple, color: Color) -> None:
+        self.text = text
+        self.font_size = font_size
+        self.position = position
+        self.color = color
+        self.font = pygame.font.SysFont(None, self.font_size)
+        self.text_surface = self.font.render(self.text, True, self.color.value)
+
+    def set_text(self, text: str) -> None:
+        self.text = text
+        self.text_surface = self.font.render(self.text, True, self.color.value)
+
+    def set_font_size(self, font_size: int) -> None:
+        self.font_size = font_size
+        self.font = pygame.font.SysFont(None, self.font_size)
+        self.text_surface = self.font.render(self.text, True, self.color.value)
+
+    def set_position(self, position: tuple) -> None:
+        self.position = position
+
+    def set_color(self, color: Color) -> None:
+        self.color = color
+        self.text_surface = self.font.render(self.text, True, self.color.value)
+
+    def get_frame(self) -> pygame.Rect:
+        return self.text_surface.get_rect(center=self.position)
+
+    def print(self, window: pygame.Surface) -> None:
+        window.blit(self.text_surface, self.get_frame())
