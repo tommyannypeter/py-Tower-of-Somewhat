094365a42c73d614d02145a07efd96567d33b485
Po-Shan Shih <tommyannypeter@gapp.nthu.edu.tw>
2023-02-11 21:26:15 +0800

    refine logger

--- logger.py
+++ logger.py
@@ -7,59 +7,33 @@ class _CustomFormatter(logging.Formatter):
     bold_red = "\x1b[31;1m"
     reset = "\x1b[0m"
 
-    text = "[%(asctime)s.%(msecs)03d][%(filename)s:%(lineno)d][%(levelname)s]: %(message)s"
-
-    FORMATS = {
-        logging.DEBUG   : grey + text + reset,
-        logging.INFO    : grey + text + reset,
-        logging.WARNING : yellow + text + reset,
-        logging.ERROR   : red + text + reset,
-        logging.CRITICAL: bold_red + text + reset
-    }
+    def __init__(self, with_color=True) -> None:
+        text = "[%(asctime)s.%(msecs)03d][%(filename)s:%(lineno)d][%(levelname)s]: %(message)s"
+        super().__init__(text, datefmt='%Y%m%d_%H:%M:%S')
+        self._with_color = with_color
 
     def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt, datefmt="%Y%m%d_%H:%M:%S")
-        return formatter.format(record)
+        # pylint: disable=W0212
+        if self._with_color:
+            colored_fmt = {
+                logging.DEBUG   : self.grey + self._fmt + self.reset,
+                logging.INFO    : self.grey + self._fmt + self.reset,
+                logging.WARNING : self.yellow + self._fmt + self.reset,
+                logging.ERROR   : self.red + self._fmt + self.reset,
+                logging.CRITICAL: self.bold_red + self._fmt + self.reset
+            }
+            self._style._fmt = colored_fmt.get(record.levelno)
+        return super().format(record)
 
-class _Logger:
-    stream_logger = None
-    file_logger = None
-    def __init__(self) -> None:
-        self.set_stream_logger()
-        self.set_file_logger()
+_stream_handler = logging.StreamHandler()
+_stream_handler.setFormatter(_CustomFormatter())
+_stream_handler.setLevel(logging.INFO)
 
-    def set_stream_logger(self) -> None:
-        self.stream_logger = logging.getLogger("stream_logger")
-        self.stream_logger.setLevel(logging.INFO)
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(_CustomFormatter())
-        self.stream_logger.addHandler(stream_handler)
+_file_handler = logging.FileHandler('tos.log', 'w+')
+_file_handler.setFormatter(_CustomFormatter(with_color=False))
+_file_handler.setLevel(logging.DEBUG)
 
-    def set_file_logger(self) -> None:
-        self.file_logger = logging.getLogger("file_logger")
-        self.file_logger.setLevel(logging.DEBUG)
-        file_handler = logging.FileHandler("tos.log", "w+")
-        self.file_logger.addHandler(file_handler)
-
-    def debug(self, string) -> None:
-        self.stream_logger.debug(string)
-        self.file_logger.debug(string)
-
-    def info(self, string) -> None:
-        self.stream_logger.info(string)
-        self.file_logger.info(string)
-
-    def warning(self, string) -> None:
-        self.stream_logger.warning(string)
-        self.file_logger.warning(string)
-
-    def error(self, string) -> None:
-        self.stream_logger.error(string)
-        self.file_logger.error(string)
-
-    def critical(self, string) -> None:
-        self.stream_logger.critical(string)
-        self.file_logger.critical(string)
-
-LOGGER = _Logger()
+LOGGER = logging.getLogger(__name__)
+LOGGER.addHandler(_stream_handler)
+LOGGER.addHandler(_file_handler)
+LOGGER.setLevel(logging.DEBUG)
