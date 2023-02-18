0b6faadbd7a5489aaa88bef34352a56b0726b978
Po-Shan Shih <tommyannypeter@gapp.nthu.edu.tw>
2023-02-18 18:05:03 +0800

    increase readability

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import pygame
 
 from config import CONFIG
@@ -8,40 +7,45 @@ from action import Action
 from opening import Opening
 from stage import Stage
 
-def should_continue(state: State) -> bool:
-    return state != State.EXIT
+class TowerOfSomewhat():
+    def __init__(self) -> None:
+        LOGGER.info("Initialize...")
+        pygame.init()
+        pygame.display.set_caption("Tower-of-Somewhat")
+        LOGGER.debug("Set window title")
+        self._window = pygame.display.set_mode((CONFIG.width, CONFIG.height))
+        LOGGER.debug("Set width and height: (%d, %d)", CONFIG.width, CONFIG.height)
 
-def next_action(state: State) -> Action:
-    match state:
-        case State.OPENING:
-            scene = Opening(name='Opening', window=window)
-        case State.STAGE:
-            scene = Stage(name='Stage', window=window)
-        case _:
-            LOGGER.error("Not implemented state: %s", str(state))
-            LOGGER.error("Quit directly")
-            return Action.QUIT
-    return scene.start()
+    def start(self) -> None:
+        current_state = State.OPENING
+        while self._should_continue(current_state):
+            current_state = self._next_state(current_state)
+        LOGGER.info("Quit Game")
+        pygame.quit()
+
+    def _should_continue(self, state: State) -> bool:
+        return state != State.EXIT
+
+    def _next_state(self, state: State) -> State:
+        match self._next_action(state):
+            case Action.START:
+                return State.STAGE
+            case Action.QUIT:
+                return State.EXIT
+
+    def _next_action(self, state: State) -> Action:
+        match state:
+            case State.OPENING:
+                scene = Opening(name='Opening', window=self._window)
+                return scene.start()
+            case State.STAGE:
+                scene = Stage(name='Stage', window=self._window)
+                return scene.start()
+            case _:
+                LOGGER.error("Not implemented state: %s", str(state))
+                LOGGER.error("Quit directly")
+                return Action.QUIT
 
 if __name__ == "__main__":
-    LOGGER.info("Initialize...")
-    pygame.init()
-
-    pygame.display.set_caption("Tower-of-Somewhat")
-    LOGGER.debug("Set window title")
-
-    window = pygame.display.set_mode((CONFIG.width, CONFIG.height))
-    LOGGER.debug("Set width and height: (%d, %d)", CONFIG.width, CONFIG.height)
-
-    current_state = State.OPENING
-
-    while should_continue(current_state):
-        match next_action(current_state):
-            case Action.START:
-                current_state = State.STAGE
-            case Action.QUIT:
-                current_state = State.EXIT
-
-    LOGGER.info("Quit Game")
-    pygame.quit()
-    sys.exit()
+    game = TowerOfSomewhat()
+    game.start()
